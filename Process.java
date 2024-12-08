72adeef70ce6529fae889388a7dd4277c10f1012
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-12-08 11:47:39 +0200

    Attribute modifications for handling quantum behavior

--- Process.java
+++ Process.java
@@ -1,11 +1,14 @@
 
+import java.util.*;
+
 public class Process {
 
-    private final int arrivalTime, burstTime, priority, quantum;
-    private final String name, color;
-    private int new_quant, remainingTime, waitTime, fcai_factor;
+    private int arrivalTime = 0, burstTime = 0, priority = 0, quantum = 0;
+    private String name = null, color = null;
+    private int used_quant, remainingTime, waitTime, fcai_factor, turnAround;
+    private final List<Integer> quantum_history;
 
-    public Process(String name, String color, int arrivalTime, int burstTime, int priority, int quantum) {
+    Process(String name, String color, int arrivalTime, int burstTime, int priority, int quantum) {
         this.name = name;
         this.color = color;
         this.arrivalTime = arrivalTime;
@@ -14,7 +17,9 @@ public class Process {
         this.quantum = quantum;
         this.priority = priority;
         this.waitTime = 0;
-        this.new_quant = 0;
+        this.used_quant = 0;
+        this.turnAround = 0;
+        this.quantum_history = new ArrayList<>();
     }
 
     // Getters start here-------------------------------------------------
@@ -42,8 +47,8 @@ public class Process {
         return quantum;
     }
 
-    public int getNewQuant() {
-        return new_quant;
+    public int getUsedQuant() {
+        return used_quant;
     }
 
     public int getPriority() {
@@ -59,23 +64,46 @@ public class Process {
     public int getWaitTime() {
         return waitTime;
     }
+
+    public int getTurnAround() {
+        return turnAround;
+    }
+
+    public List<Integer> getQuantumHistory() {
+        return quantum_history;
+    }
     // Getters end here-------------------------------------------------
 
     // Setters start here-------------------------------------------------
+    public void setFcai(int v1, int v2) {
+        fcai_factor = (int) Math.ceil((10 - priority) + (arrivalTime / v1) + (remainingTime / v2));
+    }
+
+    public void setTurnAround(int completionTime) {
+        turnAround = completionTime - arrivalTime;
+    }
+
+    // Setters end here-------------------------------------------------
+    // Updating functions start here------------------------------------ 
+    public void updateQuantumHistory(int updated_quantum) {
+        quantum_history.add(updated_quantum);
+    }
+
+    public void resetUsedQuant() {
+        used_quant = 0;
+    }
+
     public void decrementRemainingTime() {
         this.remainingTime--;
     }
 
-    public void setFcai(int v1, int v2) {
-        this.fcai_factor = (int) Math.ceil((10 - priority) + (arrivalTime / v1) + (remainingTime / v2));
-    }
-
-    public void updateQuantum() {
-        this.new_quant++;
+    public void updateUsedQuantum() {
+        used_quant++;
     }
 
     public void incrementWaitTime() {
-        this.waitTime++;
+        waitTime++;
     }
-    // Setters end here-------------------------------------------------
+
+    // Updating functions end here--------------------------------------
 }
